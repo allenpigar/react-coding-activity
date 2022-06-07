7453e92636e455be7d286e9eff8b6f8a9bef55b8
Allen Pigar <50983144+allenpigar@users.noreply.github.com>
2022-06-07 20:57:10 +0800

    I added some for example
    for example use only

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import InnerHtml from '../components/InnerHtml'
 export default function Home({ data }) {
   return (
     <>
+      <div>I add this</div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
         {data.map((item, i) => (
           <div key={i} className="p-8">
